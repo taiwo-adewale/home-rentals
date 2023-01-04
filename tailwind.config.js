b487b3b27f5a723619f498d2a3c4d6dc01dcfeea
Taiwo Adewale <adewaletaiwo08@gmail.com>
2023-01-04 20:29:45 +0100

    set-up styles

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      xs: { max: "339px" },
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primary: "F4511E",
+        secondary: "#18191F",
+        gray: "#818181",
+        dark: "#0A142F",
+        altGray: "rgba(10,20,47,0.5)",
+      },
+      backgroundColor: {
+        mainBg: "#F9F9FE",
+      },
+    },
+    fontFamily: {
+      primary: ["Josefin Sans", "sans-serif"],
+      secondary: ["Source Serif Pro", "serif"],
+    },
+  },
+  plugins: [],
+};
